c53aeb575867eb34b108ead4fc8ab0313798b000
MeghlaB <meghlab964@gmail.com>
2026-02-10 22:19:52 +0600

    Folder sturctare done

---
+++ next.config.ts
@@ -0,0 +1,24 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/auth/:path*",
+        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/:path*`,
+      },
+    ];
+  },
+};
+export default nextConfig;
+
+
